f2b638144c72d5c1ebfde0a58fe72e97bf587e0a
Joe <schneiderjg4339@gmail.com>
2025-10-05 12:54:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/*--------------------------------------------------*/
+//NAME:           Joseph Schneider
+//FILE:           main.cpp
+//CLASS:          CICS-377 Operating Systems
+//ASSIGNMENT:     Project_01 UNIX Shell
+//PROFESSOR:      Nikko
+//DATE COMPLETED: 09/29/2025
+//DATE DUE:       10/02/2025
+/*--------------------------------------------------*/
+
 #include <tsh.h>
 
 /**
